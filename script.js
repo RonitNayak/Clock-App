672cf266d850bd771ed0beb8c325fd30543d44b4
Ronit N <ronit@nayak.ca>
2023-01-23 21:59:58 -0500

    optimized code using switch, ternary, etc

--- script.js
+++ script.js
@@ -1,60 +1,40 @@
-function postTime() {
+function updateClock() {
 
-  var myDate = new Date();
-      hours = myDate.getHours();
-      minutes = myDate.getMinutes();
-      seconds = myDate.getSeconds();
-      month = myDate.getMonth();
-      date = myDate.getDate();
-      day = myDate.getDay();
-      year = myDate.getFullYear();
+  const myDate = new Date();
+  const militaryHours = myDate.getHours();
+  let hours = militaryHours > 12 ? militaryHours - 12 : militaryHours;
+  hours = hours === 00 ? 12 : hours.toString().padStart(2, '0');
+  const minutes = myDate.getMinutes().toString().padStart(2, '0');
+  const seconds = myDate.getSeconds().toString().padStart(2, '0');
+  const month = myDate.getMonth();
+  const date = myDate.getDate();
+  const day = myDate.getDay();
+  const year = myDate.getFullYear();
 
-  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-  var monthprint = months[month];
-  var dayprint = days[day];
-  var ordinal = "";
-  var ampm = "";
+  const ordinal = getOrdinal(date);
+  const ampm = militaryHours > 12 ? 'PM' : 'AM';
 
-  if (date== 1) {
-    ordinal = "st"
-  } else if (date==2) {
-    ordinal = "nd"
-  } else if (date==3) {
-    ordinal = "rd"
-  } else if (date==21) {
-    ordinal = "st"
-  } else if (date==22) {
-    ordinal = "nd"
-  } else if (date==23) {
-    ordinal = "rd"
-  } else if (date==31) {
-    ordinal = "st"
-  } else {
-    ordinal = "th"
-  }
-
-  if (hours<12) {
-    ampm = "AM"
-  } else {
-    (hours=hours-12), ampm = "PM";
-  }
-
-  function zeroNeeded(x) {
-    x = x.toString();
-    if (x.length<2) {
-      numprevious = "0"
-    } else {
-      numprevious =""
-    }
-    return numprevious;
-  }
-
-  document.getElementById("time").innerHTML = zeroNeeded(hours) + hours + ":" + zeroNeeded(minutes) + minutes + ":" + zeroNeeded(seconds) + seconds + " " + ampm;
-  document.getElementById("date").innerHTML = dayprint + ", " + monthprint +" "+ date + ordinal + " "+ year;
-
+  const timeDisplay = document.getElementById("time")
+  const dateDisplay = document.getElementById("date")
+  timeDisplay.textContent = `${hours}:${minutes}:${seconds} ${ampm}`
+  dateDisplay.textContent = `${days[day]}, ${months[month]} ${date}${ordinal} ${year}`
 }
 
-setTimeout(postTime,0);
-setInterval(postTime, 1000);
+function getOrdinal(date) {
+  if (date > 3 && date < 21) return 'th';
+
+  switch (date % 10) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    case 3:
+      return 'rd';
+  }
+}
+
+updateClock();
+setInterval(updateClock, 1000);
